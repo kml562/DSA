ccd078b82efc37d5d8deda1823b173f8a1d22750
Kamal Bisht <111355794+kml562@users.noreply.github.com>
2023-03-31 23:27:31 +0530

    do

--- week1/prb2.js
+++ week1/prb2.js
@@ -8,53 +8,47 @@
 
 // Your task is to simulate the execution of the algorithm for a given value of n
 
-
-
-
-const check=(n,arr)=>{
-//     if(arr.length==1)
-// {
-//     return
-// } 
-arr.sort((a,b)=> a-b)
-// console.log(arr)
- for(let i= 0;i<n;i++){
-    if(arr[i]!=i+1){
-        console.log(i+1);
-        break
-    } 
- }
-}
+const check = (n, arr) => {
+  //     if(arr.length==1)
+  // {
+  //     return
+  // }
+  arr.sort((a, b) => a - b);
+  // console.log(arr)
+  for (let i = 0; i < n; i++) {
+    if (arr[i] != i + 1) {
+      console.log(i + 1);
+      break;
+    }
+  }
+};
 // check(10,[2 ,8, 10 ,6, 5 ,1, 3, 7, 4])
 
-
-
 function runProgram(input) {
-    // Write code here
-   input= input.split("\n");
-   let n= +input[0];
-   let arr= input[1].split(" ").map(Number);
-   check(n,arr)
-  }
-  
-  if (process.env.USER === "") {
-    runProgram(``);
-  } else {
-    process.stdin.resume();
-    process.stdin.setEncoding("ascii");
-    let read = "";
-    process.stdin.on("data", function (input) {
-      read += input;
-    });
-    process.stdin.on("end", function () {
-      read = read.replace(/\n$/, "");
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-    });
-    process.on("SIGINT", function () {
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-      process.exit(0);
-    });
-  }
-  
+  // Write code here
+  input = input.split("\n");
+  let n = +input[0];
+  let arr = input[1].split(" ").map(Number);
+  check(n, arr);
+}
+
+if (process.env.USER === "") {
+  runProgram(``);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
---
+++ week1/prb3.js
@@ -0,0 +1,27 @@
+
+ let   fullStack={
+        name:"full stack web devloper",
+        fee: 4999,
+        location : "UK"
+    }
+
+    if(fullStack.fee<5000){
+        console.log(true)
+    }else{
+        console.log(fasle)
+    }
+    (fullStack.fee<5000?console.log(true):console.log(false))
+
+    function fun(a,b){
+        return a + b
+        console.log("ldfj")
+    }
+    const fun=(a,b)=>{
+return a+b
+    }
+
+
+ let a="dfljk"
+   function kdj(){
+    let a="dlldf"
+   }
