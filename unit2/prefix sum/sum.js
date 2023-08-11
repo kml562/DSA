4ce08fe3b56cb6e6b7bc2b10b29cf97d9c56ec46
Kamal Bisht <111355794+kml562@users.noreply.github.com>
2023-08-11 12:12:30 +0530

    done

---
+++ unit2/prefix sum/sum.js
@@ -0,0 +1,35 @@
+// Given an array having both positive and negative integers. The task is to compute the length of the largest 
+//subarray with sum 0.
+
+// Example 1:
+
+// Input:
+// N = 8
+// A[] = {15,-2,2,-8,1,7,10,23}
+// Output: 5
+// Explanation: The largest subarray with
+// sum 0 will be -2 2 -8 1 7.
+// Your Task:
+// You just have to complete the function maxLen() which takes two arguments an array A and n, where n is the size
+// of the array A and returns the length of the largest subarray with 0 sum.
+
+// Expected Time Complexity: O(N).
+// Expected Auxiliary Space: O(N).
+
+function maxLen(arr, n) {
+  let sum = 0;
+  let maxLen = 0;
+  let hashMap = new Map();
+  hashMap.set(0, -1);
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    if (hashMap.has(sum)) {
+      maxLen = Math.max(maxLen, i - hashMap.get(sum));
+    } else {
+      hashMap.set(sum, i);
+    }
+  }
+
+  return maxLen;
+}
